32c647aa2155e3952c18f7a245aa51cd83b1bb2b
robertmirro <rmirro@gmail.com>
2014-06-16 12:00:53 -0400

    ignore WS .idea directory
    re-factor, separate parsing logic
    
    cleanup code

--- windows-test/my_parser.js
+++ windows-test/my_parser.js
@@ -1,38 +1,23 @@
-// Load the fs (filesystem) module
+// Require my new parser.js file.
+var Parser = require('./parser');
+
+// Load the fs (filesystem) module.
 var fs = require('fs');
 
 // Read the contents of the file into memory.
 fs.readFile('example_log.txt', function (err, logData) {
 
     // If an error occurred, throwing it will
-    // display the exception and end our app.
+    // display the exception and kill our app.
     if (err) throw err;
 
     // logData is a Buffer, convert to string.
     var text = logData.toString();
 
-    console.log('text:\n' , text );
-    console.log('logData:\n', logData );
+    // Create an instance of the Parser object.
+    var parser = new Parser();
 
-    var results = {};
-
-    // Break up the file into lines.
-    var lines = text.split('\n');
-
-    console.log('lines:\n' , lines );
-
-    lines.forEach(function(line) {
-        var parts = line.split(' ');
-        var letter = parts[1];
-        var count = parseInt(parts[2]);
-
-        if(!results[letter]) {
-              results[letter] = 0;
-            }
-
-        results[letter] += parseInt(count);
-      });
-
-    console.log(results);
-  // { A: 2, B: 14, C: 6 }
+    // Call the parse function.
+    console.log('Grades: ', parser.parse(text));
+    // { A: 2, B: 14, C: 6 }
 });
