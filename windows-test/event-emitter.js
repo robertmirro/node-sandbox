1aa32a2f4b93bea2a5130d713ecb609cfec445e7
robertmirro <rmirro@gmail.com>
2014-06-22 19:39:19 -0400

    emit an additional value

--- windows-test/event-emitter.js
+++ windows-test/event-emitter.js
@@ -2,20 +2,22 @@ var EventEmitter = require('events').EventEmitter;
 var util = require('util');
 
 function MyClass() {
+    console.log(this instanceof MyClass);
     if (!(this instanceof MyClass)) return new MyClass();
 
     EventEmitter.call(this);
 
     var self = this;
-    setTimeout(function timeoutCb() {
-        self.emit('myEvent', 'hello world', 42);
+    //setTimeout(function timeoutCb() {
+    setTimeout(function () {
+        self.emit('myEvent', 'hello world', 44, 84);
     }, 1000);
 }
 util.inherits(MyClass, EventEmitter);
 
 var myObj = new MyClass();
 var start = Date.now();
-myObj.on('myEvent', function myEventCb(str, num) {
-    console.log('myEvent triggered', str, num, Date.now() - start);
+myObj.on('myEvent', function myEventCb(str, num, num2) {
+    console.log('myEvent triggered: ', str, num, num2, Date.now() - start);
 });
 
