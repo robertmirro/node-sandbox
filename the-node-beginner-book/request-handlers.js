830918fbe78daf25bec7df5f9a8ee7027635b573
robertmirro <rmirro@gmail.com>
2014-06-25 17:04:56 -0400

    implement POST data handling logic

--- the-node-beginner-book/request-handlers.js
+++ the-node-beginner-book/request-handlers.js
@@ -1,33 +1,40 @@
 var exec = require('child_process').exec;
+var querystring = require('querystring');
 
 // receive response object from router
-function start(response) {
+function start(response, postData) {
     "use strict";
 
     console.log('  **Request handler "start" was called...', new Date());
 
-    var content = 'empty';
-    // use child_process.exec to execute a shell command in a non-blocking fashion
-    // exec('find /', function( error, stdout, stderr ) {
-    // exec('ls -alh /', function( error, stdout, stderr ) {
-    exec('find /', {timeout: 10000, maxBuffer: 20000*1024}, function( error, stdout, stderr ) {
-        //console.log('Hello World from... START! --- ', stdout);
-        console.log('  **Request handler "start" is DONE...', new Date());
+    var html_content = '<html>'
+        + '<head>'
+        + '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> '
+        + '</head>'
+        + '<form action="/upload" method="post">'
+        + '<textarea name="text" rows="15" cols="60"></textarea><br />'
+        + '<input type="submit" value="Submit Me" />'
+        + '</form>'
+        + '</body>'
+        + '</html>'
+        ;
 
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write('Hello World from... START! --- ' + new Date() + '\n' + stdout);
-        response.end();
-    })
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(html_content);
+    response.end();
 }
 
 // receive response object from router
-function upload(response) {
+function upload(response, postData) {
     "use strict";
 
     console.log('  **Request handler "upload" was called...');
 
+    postData = querystring.parse(postData).text || 'No data has been posted.';
+
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write('Hello World from... UPLOAD! --- ' + new Date());
+    response.write('\nYou have posted the following data:\n\n' + postData /* querystring.parse(postData).text */ );
     response.end();
 }
 
--- the-node-beginner-book/router.js
+++ the-node-beginner-book/router.js
@@ -1,13 +1,13 @@
 
 // receive response object from server
-function route(handle, pathname, response) {
+function route(handle, pathname, response, postData) {
     "use strict";
 
     console.log('  **Routing request for ' + pathname);
 
     if (typeof handle[pathname] === 'function') {
         // pass response object along to request handler
-        return handle[pathname](response);
+        return handle[pathname](response, postData);
     } else {
         console.log('  ****No request handler found for ' + pathname);
         // need to output 404 content from this router now that server no longer handles outputting content
