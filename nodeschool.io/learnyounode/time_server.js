9122318d74e7381d0b48d93f3ffc47ced8891935
robertmirro <rmirro@gmail.com>
2014-06-30 16:26:43 -0400

    time server

---
+++ nodeschool.io/learnyounode/time_server.js
@@ -0,0 +1,54 @@
+var net = require('net');
+
+var listenOnPort = process.argv[2];
+
+function getDateTime() {
+    "use strict";
+
+    var seperatorDate = '-';
+    var seperatorTime = ':'
+    var today = new Date;
+
+    return (today.getFullYear()
+        + seperatorDate
+        + (today.getMonth() + 1 > 9 ? '' : '0') + (today.getMonth() + 1)
+        + seperatorDate
+        + (today.getDate() > 9 ? '' : '0')  + today.getDate()
+        + ' '
+        + (today.getHours() > 9 ? '' : '0')  + today.getHours()
+        + seperatorTime
+        + (today.getMinutes() > 9 ? '' : '0')  + today.getMinutes()
+        );
+}
+
+var server = net.createServer( function( socket ) {
+    //console.log('Connected to server...', getDateTime());
+    socket.write(getDateTime() + '\n');
+    socket.end();
+});
+server.listen(listenOnPort);
+
+//console.log('Server listening on port', server.address().port );
+
+// OFFICIAL SOLUTION
+//
+//var net = require('net')
+//
+//function zeroFill(i) {
+//    return (i < 10 ? '0' : '') + i
+//}
+//
+//function now () {
+//    var d = new Date()
+//    return d.getFullYear() + '-'
+//        + zeroFill(d.getMonth() + 1) + '-'
+//        + zeroFill(d.getDate()) + ' '
+//        + zeroFill(d.getHours()) + ':'
+//        + zeroFill(d.getMinutes())
+//}
+//
+//var server = net.createServer(function (socket) {
+//    socket.end(now() + '\n')
+//})
+//
+//server.listen(Number(process.argv[2]))
