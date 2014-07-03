48f154b367e909c140f504aa58e4d7fa774897ea
robertmirro <rmirro@gmail.com>
2014-07-03 19:42:36 -0400

    pipe stdin to stdout

---
+++ nodeschool.io/stream-adventure/input_output.js
@@ -0,0 +1,7 @@
+
+// take data entered from stdin and pipe it right back out to stdout (simulate echo)
+process.stdin.pipe(process.stdout);
+
+// OFFICIAL SOLUTION
+//
+//process.stdin.pipe(process.stdout);
