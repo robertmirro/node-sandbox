0d10f1d1a5e33cc0e8c224d596b48e3477487eb0
robertmirro <rmirro@gmail.com>
2014-07-06 15:45:11 -0400

    lines, convert odd lines to lowercase and even lines to uppercase using stdin, stdout, split and through

---
+++ nodeschool.io/stream-adventure/lines.js
@@ -0,0 +1,78 @@
+var through = require('through');
+var split = require('split');
+
+var lineNum = 0;
+var tr = through(function (line) {
+    line = line.toString();
+    this.queue( ( lineNum++ % 2 === 0 ? line.toLowerCase() : line.toUpperCase() ) + '\n' );
+});
+process.stdin.pipe(split()).pipe(tr).pipe(process.stdout);
+
+
+//// this is my solution and it works but use official solution above because it makes better use of pipe() and through()
+//var lineNum = 0;
+//process.stdin
+//    .pipe(split())
+//    .pipe( through( function writeLine( line ) {
+//        // convert even numbered lines to UPPERCASE and odd numbered lines to LOWERCASE, consider first line to be odd
+//        line = (++lineNum % 2 === 0) ? line = line.toUpperCase() : line = line.toLowerCase();
+//        process.stdout.write(line.toString() + '\n');
+//    }));
+
+
+//var lineNum = 0;
+//process.stdin.pipe(split()).on( 'data' , function( line ) {
+//    // only convert even numbered lines to UPPERCASE, consider first line to be odd
+//    if (++lineNum % 2 === 0) {
+//        line = line.toUpperCase();
+//    }
+//
+//    //process.stdout.write(line.toString());
+//    //console.log( ++lineNum + ' ' + line);
+//    console.log( line );
+//});
+
+
+//// print out a line for each line in input data
+//var i = 0;
+//process.stdin.pipe(split()).on( 'data' , function( line ) {
+//    console.log(++i , line);
+//});
+
+
+// CAN TEST AS FOLLOWS:
+//1
+//$ echo -e 'bob\nmirro\nis\nme' | node lines.js
+//BOB
+//MIRRO
+//IS
+//ME
+// --OR--
+//2
+//node lines.js
+//[type characters and press <enter> after each line]
+//bob
+//BOB
+//mirro
+//MIRRO
+//is
+//IS
+//me
+//ME
+
+
+// OFFICIAL SOLUTION
+//
+//var through = require('through');
+//var split = require('split');
+//
+//var lineCount = 0;
+//var tr = through(function (buf) {
+//    var line = buf.toString();
+//    this.queue(lineCount % 2 === 0
+//            ? line.toLowerCase() + '\n'
+//            : line.toUpperCase() + '\n'
+//    );
+//    lineCount ++;
+//});
+//process.stdin.pipe(split()).pipe(tr).pipe(process.stdout);
