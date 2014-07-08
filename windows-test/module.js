b4a5209fe1995d5643ad44a86c22200654bcb5ce
robertmirro <rmirro@gmail.com>
2014-07-08 16:41:50 -0400

    module.exports vs exports

---
+++ windows-test/module.js
@@ -0,0 +1,28 @@
+var mod = require('module');
+
+var holdModuleExports = module.exports;
+
+function myModule (  ) {
+    console.log( 'myModule' );
+}
+function writeLog ( title ) {
+    console.log('\n---------- ' + title + ' ----------');
+    console.log( module );
+    console.log('');
+    console.log( exports );
+    console.log('');
+    console.log( 'module.exports === exports:' , module.exports === exports );
+}
+
+writeLog('init');
+
+module.exports = myModule;
+writeLog('module.exports = myModule');
+
+// reset module.exports to its default value
+module.exports = holdModuleExports;
+
+exports.myMod = myModule;
+exports.myOtherMod = myModule;
+writeLog('exports.myMod = myModule...');
+
