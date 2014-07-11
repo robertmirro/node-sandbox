9eae58c6e77a371d4c69abc1c38b304f2b2415cf
robertmirro <rmirro@gmail.com>
2014-07-11 15:09:36 -0400

    static file server

--- nodejs-in-action/chat/server.js
+++ nodejs-in-action/chat/server.js
@@ -4,3 +4,24 @@ var path = require('path');
 var mime = require('mime');
 
 var cache = {};
+
+function send404 ( response ) {
+    "use strict";
+    response.writeHead( 404 , {'Content-Type': 'text/plain'} );
+    response.end( 'Error 404: Resource not found.' );
+}
+
+function sendFile ( response , filePath , fileContents ) {
+    "use strict";
+    response.writeHead( 200 , {'Content-Type': mime.lookup( path.basename( filePath ))} );
+    response.end( fileContents );
+}
+
+function serverStaticFile ( response , cache , absoluteFilePath ) {
+    "use strict";
+    // server file from cache if possible
+    if ( cache[absoluteFilePath] ) {
+        sendFile( response , absoluteFilePath , cache[absoluteFilePath] );
+        return;
+    }    
+}
