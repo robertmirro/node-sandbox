b64f039981d38c85ba49c36d72c060190ec2de6c
robertmirro <rmirro@gmail.com>
2014-07-15 18:26:13 -0400

    complete websocket client/server testing

--- nodeschool.io/stream-adventure/websockets.js
+++ nodeschool.io/stream-adventure/websockets.js
@@ -1,3 +1,11 @@
 var websocket = require('websocket-stream');
 
-var stream = websocket( 'ws://localhost:8000' );
+var stream = websocket( 'ws://localhost:' + (process.argv[2] || 8080) );
+//stream.write( 'write: ' + new Date() );
+stream.end( 'hello\n' );
+
+// OFFICIAL SOLUTION
+//
+//var ws = require('websocket-stream');
+//var stream = ws('ws://localhost:8000');
+//stream.end('hello\n');
--- nodeschool.io/stream-adventure/websockets_server.js
+++ nodeschool.io/stream-adventure/websockets_server.js
@@ -2,14 +2,27 @@ var http = require('http');
 var websocket = require('websocket-stream');
 var WebSocketServer = require('ws').Server;
 
-var listenOnPort = process.argv[2];
+var listenOnPort = process.argv[2] || 8080;
 
 var httpServer = http.createServer();
-httpServer.listen(listenOnPort);
+httpServer.listen( listenOnPort , function() {
+    console.log( 'Server listening on port: %s', httpServer.address().port );
+});
 
 // piggy back websocket server on http server
 var wsServer = new WebSocketServer( { server: httpServer} );
 wsServer.on( 'connection' , function( ws ) {
+    console.log( 'websocket server connection...' );
+
+    // open websocket stream on websocket connection
     var wsStream = websocket( ws );
-    console.log( 'wsServer connection' );
+    wsStream.on( 'data' , function( data ) {
+        console.log( 'data received: %s' , data );
+    });
+
+//    // can also listen for 'message' event directly on websocket connection
+//    // this is an alternate to stream method above that produces same effect
+//    ws.on( 'message' , function( message ) {
+//        console.log( 'message received: %s' , message );
+//    });
 });
