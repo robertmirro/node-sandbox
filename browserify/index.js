53b85d410b7166addd78e979835decda7a528d9f
robertmirro <rmirro@gmail.com>
2014-07-16 18:44:32 -0400

    test browserified version of index.js in browser

--- browserify/index.js
+++ browserify/index.js
@@ -1,7 +1,14 @@
 var uniq = require('uniq');
 
 var arrayDups = [ 1 , 2 , 2 , 3 , 4 , 5 , 5 , 5 , 7 , 7 , 8 ];
-var arrayUniq = arrayDups.slice();
+var arrayUniq = arrayDups.slice( 0 );
 
+// chrome console does not display correct output if console is opened AFTER html is rendered AND "uniq( arrayUniq )" is NOT used
+// if using "uniq( arrayDups )" below, open console BEFORE rendering html OR re-render html after console is opened
 console.log( 'duplicate array data: ' , arrayDups );
-console.log( 'unique array data: ' , uniq( arrayUniq ) );
+console.log( 'unique array data: ' , uniq( /* arrayUniq */ arrayDups ) );
+
+// output
+//
+//duplicate array data:  [1, 2, 2, 3, 4, 5, 5, 5, 7, 7, 8]
+//unique array data:  [1, 2, 3, 4, 5, 7, 8]
---
+++ browserify/index_browserify.js
@@ -0,0 +1,75 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var uniq = require('uniq');
+
+var arrayDups = [ 1 , 2 , 2 , 3 , 4 , 5 , 5 , 5 , 7 , 7 , 8 ];
+var arrayUniq = arrayDups.slice( 0 );
+
+// chrome console does not display correct output if console is opened after html is rendered AND "uniq( arrayUniq )" is NOT used
+// if using "uniq( arrayDups )" below, open console BEFORE rendering html OR re-render html after console is open
+console.log( 'duplicate array data: ' , arrayDups );
+console.log( 'unique array data: ' , uniq( /* arrayUniq */ arrayDups ) );
+
+// output
+//
+//duplicate array data:  [1, 2, 2, 3, 4, 5, 5, 5, 7, 7, 8]
+//unique array data:  [1, 2, 3, 4, 5, 7, 8]
+},{"uniq":2}],2:[function(require,module,exports){
+"use strict"
+
+function unique_pred(list, compare) {
+  var ptr = 1
+    , len = list.length
+    , a=list[0], b=list[0]
+  for(var i=1; i<len; ++i) {
+    b = a
+    a = list[i]
+    if(compare(a, b)) {
+      if(i === ptr) {
+        ptr++
+        continue
+      }
+      list[ptr++] = a
+    }
+  }
+  list.length = ptr
+  return list
+}
+
+function unique_eq(list) {
+  var ptr = 1
+    , len = list.length
+    , a=list[0], b = list[0]
+  for(var i=1; i<len; ++i, b=a) {
+    b = a
+    a = list[i]
+    if(a !== b) {
+      if(i === ptr) {
+        ptr++
+        continue
+      }
+      list[ptr++] = a
+    }
+  }
+  list.length = ptr
+  return list
+}
+
+function unique(list, compare, sorted) {
+  if(list.length === 0) {
+    return list
+  }
+  if(compare) {
+    if(!sorted) {
+      list.sort(compare)
+    }
+    return unique_pred(list, compare)
+  }
+  if(!sorted) {
+    list.sort()
+  }
+  return unique_eq(list)
+}
+
+module.exports = unique
+
+},{}]},{},[1])
