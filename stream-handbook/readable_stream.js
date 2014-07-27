adbb356fa35e2bf098a55fe665f1c9a800f1e2c4
robertmirro <rmirro@gmail.com>
2014-07-27 19:00:07 -0400

    comment stream functionality

--- stream-handbook/readable_stream.js
+++ stream-handbook/readable_stream.js
@@ -2,8 +2,14 @@ var Readable = require('stream').Readable;
 
 // simple readable stream that pipes output to stdout
 var readableStream = new Readable;
+
+// pushing data to read stream before piping it to stdout below
+// this results in data chunks being buffered until consumer of
+//   the read stream is ready to read them
 readableStream.push( 'one ');
 readableStream.push( 'two\n');
+
+// null terminator to inform consumer that data is done being output
 readableStream.push( null );
 
 readableStream.pipe( process.stdout );
