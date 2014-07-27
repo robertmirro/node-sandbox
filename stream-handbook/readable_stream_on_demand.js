f63f7a382464a3da8d69b14dc14d2125064ef6e4
robertmirro <rmirro@gmail.com>
2014-07-27 19:43:52 -0400

    comment out debugging code

--- stream-handbook/readable_stream_on_demand.js
+++ stream-handbook/readable_stream_on_demand.js
@@ -8,6 +8,7 @@ var charCode = 97;
 // specify internal _read function to respond each time consumer is ready to read data
 // ignore "size" param used to specify how many bytes consumer wants to read
 readableStream._read = function( size ) {
+//    console.log( '\nc=%s' , String.fromCharCode( charCode ) );
     readableStream.push( String.fromCharCode( charCode++ ) );
     if ( charCode > 'z'.charCodeAt(0) ) {
         // null terminator to inform consumer that data is done being output
