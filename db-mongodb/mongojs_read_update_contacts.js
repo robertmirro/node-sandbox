1e08c24e8e59733c81a31079b3e3c03c2d3935a8
robertmirro <rmirro@gmail.com>
2014-08-03 18:10:41 -0400

    experiment with .update()

--- db-mongodb/mongojs_read_update_contacts.js
+++ db-mongodb/mongojs_read_update_contacts.js
@@ -36,7 +36,12 @@ httpServer.on( 'request' , function( request , response ) {
 //            response.write( JSON.stringify( contact , null , ' ' ) + '<br /><br />' );
             response.write( '<p>' + contact.name + ' &lt;' + contact.email + '&gt; (<b>' + contact.age + '</b>) </p>' );
 
-//            db.contacts.save( { _id : contact._id , name : contact.name + ' (' + contact.index + ')'} );
+//            // .save() ONLY saved the _id and name fields and seemed to lose all other fields in record
+//            db.contacts.save( { _id : contact._id , name : contact.name + ' (' + contact.index + ')' } );
+
+//            // .update() worked as desired and updated .name field
+//            // .update() also seems to have re-written entire record because record keys are now stored alphabetically
+//            db.contacts.update( { _id : contact._id } , { $set: { name : contact.name + ' (' + contact.index + ')' } } );
         });
         response.end( '</body></html>' );
     });
