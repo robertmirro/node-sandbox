a1c3d45910ffc8f214d36e5444ad3419c68df370
robertmirro <rmirro@gmail.com>
2014-08-04 18:55:49 -0400

    express accessing mongodb via mongoose, init setup

---
+++ node-express-2014/expMongoDB/app.js
@@ -0,0 +1,67 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var http = require('http');
+
+var routes = require('./routes/index');
+// var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+//app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+//    app.use(express.errorHandler());
+//    mongoose.connect('mongodb://55.55.55.5/mongo');
+}
+
+app.get('/', function(req, res) {
+    res.send( 'ok' );
+});
+
+
+/*
+//load all files in models dir
+fs.readdirSync(__dirname + '/models').forEach(function(filename) {
+    if (~filename.indexOf('.js')) require(__dirname + '/models/' + filename)
+});
+
+
+app.get('/users', function(req, res) {
+    mongoose.model('users').find(function(err, users) {
+        res.send(users);
+    });
+});
+
+app.get('/posts/:userId', function(req, res) {
+    mongoose.model('posts').find({user: req.params.userId}, function(err, posts) {
+        mongoose.model('posts').populate(posts, {path: 'user'}, function(err, posts) {
+            res.send(posts);
+        });
+    });
+});
+*/
+
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
