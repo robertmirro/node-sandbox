56729943cf24c7c49384948ef24712f501665d12
robertmirro <rmirro@gmail.com>
2014-08-03 20:18:39 -0400

    experiment with express routes

--- node-express-2014/exp2014/routes/index.js
+++ node-express-2014/exp2014/routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    // access querystring params
+    // test: http://localhost:3000/?title=NoTitle&name=Roberto
+    // result: querystring: { title: 'NoTitle', name: 'Roberto' }
+    console.log( 'querystring:' , req.query );
+
+    // res.send( 'ok' );  // returns content-type text/html and displays "ok"
+    // res.send( 200 );  // returns content-type text/plain and HTTP status 200
+    // res.send({
+    //     users : [ 'Robert' , 'Bob' , 'Bobbio' ]  // returns content-type application/json and JSON object
+    // });
+    res.render('index', { title: 'Express' , name: 'El Bobbio' });  // render Hogan template (views/index.hjs)
+});
+
+router.get('/users/:id', function(req, res) {
+    // access querystring params
+    // test: http://localhost:3000/users/48
+    // result: params: { id: '48' }
+    console.log( 'params:' , req.params );
+
+    res.send( 'id:' + req.params.id , 200 );  // returns content-type text/plain and HTTP status 200, display id:48 in browser
 });
 
 module.exports = router;
