c7074244f27cabdccf372e4ae58009f159997cec
robertmirro <rmirro@gmail.com>
2014-08-06 11:35:39 -0400

    files used for webstorm tuts

---
+++ windows-test/webstorm_tut.js
@@ -0,0 +1,65 @@
+
+
+var bob = 'mirro';
+function testMe( param1 , param2 , bobbio ) {
+    "use strict";
+    return 'yo mama';
+}
+testMe( xxx , bob , ROB )
+testMe( bob , bob , ROB )
+testMe( bob , bob , ROB )
+bob
+bob;
+
+var mirro2;
+mirro = '1';
+
+
+
+
+ctrl + shift + v = paste from recent buffers
+
+
+
+
+
+var yoiu;
+CTRL + SHIFT + V = Paste from recent buffers
+bob;
+bob;
+bob;
+test;Me( bob , bob )
+bob;
+testMe( bob , bob , ROB )
+bob;
+testMe( bob , bob , ROB )
+testMe( bob , bob , ROB )
+bob;
+process.stdout.write( data.toString() );
+bob;
+
+testMe( bob , bob , ROB )
+testMe( bob , bob , ROB )
+
+
+try {
+    var mirro = 'bob';
+} catch ( e ) {
+} finally {
+}
+
+
+if ( ) {
+    var b;
+    var c;
+    if () {
+        var d;
+    }
+}
+
+if ( ) {
+    bob;
+    testMe( bob , bob , ROB )
+} else {
+}
+
