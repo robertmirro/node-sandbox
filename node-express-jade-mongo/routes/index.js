45a5221869d92aea12d330b1080cd6107c14afd4
robertmirro <rmirro@gmail.com>
2014-08-10 16:38:18 -0400

    create user interface, insert into users collection

--- node-express-jade-mongo/routes/index.js
+++ node-express-jade-mongo/routes/index.js
@@ -20,4 +20,33 @@ router.get('/users', function(req, res) {
     });
 });
 
+/* GET "add new user" page. */
+router.get('/createuser', function(req, res) {
+    res.render('createuser', { title: 'Add New User' });
+});
+
+/* POST to "add user" service */
+router.post('/adduser', function(req, res) {
+//    var db = req.db;
+    var collection = req.db.get( 'users' );
+    
+    // get submitted form fields
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+    console.log( 'userName:%s , userEmail:%s' , userName , userEmail );
+    
+    collection.insert(
+        {
+            username: userName ,
+            email: userEmail
+        } ,
+        function (err, document) {
+            console.log( 'ERROR:' , err );
+//            res.send( 'Uh oh...' );
+            res.location( 'users' );
+            res.redirect( 'users' );
+        }
+    );
+});
+
 module.exports = router;
