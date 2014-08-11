8c973b69d367992b673665f73cfdd2cc6f667ad6
robertmirro <rmirro@gmail.com>
2014-08-11 19:47:31 -0400

    user list route, return as json data to be used by spa

--- node-express-jade-mongo/routes/users.js
+++ node-express-jade-mongo/routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+/* GET list of users from DB, return as JSON data */
+router.get( '/userslist' , function ( req , res ) {
+    req.db.collection( 'users').find().toArray( function( err , records ) {
+        res.json( records );
+    });
 });
 
 module.exports = router;
