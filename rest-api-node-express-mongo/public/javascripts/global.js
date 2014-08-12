01b39efe6bf39164bc6ba8f18da9f67e4761381d
robertmirro <rmirro@gmail.com>
2014-08-12 19:08:18 -0400

    add user functionality

--- rest-api-node-express-mongo/public/javascripts/global.js
+++ rest-api-node-express-mongo/public/javascripts/global.js
@@ -3,8 +3,11 @@ var userListData = [];
 $( document ).ready( function() {
     populateTable();
 
-    // attach click handler to all username links
+    // attach click handler to all username links to display user details
     $( '#userList table tbody' ).on( 'click' , 'td a.linkshowuser' , showUserInfo );
+
+    // add user button click handler to post to /users/adduser
+    $( '#btnAddUser' ).on( 'click' , addUser );
 });
 
 function populateTable() {
@@ -49,4 +52,50 @@ function showUserInfo( event ) {
     $( '#userAge' ).text( thisUserObject.age );
     $( '#userGender' ).text( thisUserObject.gender );
     $( '#userLocation' ).text( thisUserObject.location );
+}
+
+function addUser( event ) {
+    event.preventDefault();
+
+    var errorCount = 0;
+    $( '#addUser input' ).each( function( index , val ) {
+        if( $(this).val() === '' ) {
+            errorCount++;
+        }
+    });
+
+    if( errorCount !== 0 ) {
+        return alert( 'Please fill in all fields' );
+    }
+
+    // If it is, compile all user info into one object
+    var newUser = {
+        'username': $('#addUser fieldset input#inputUserName').val(),
+        'email': $('#addUser fieldset input#inputUserEmail').val(),
+        'fullname': $('#addUser fieldset input#inputUserFullName').val(),
+        'age': $('#addUser fieldset input#inputUserAge').val(),
+        'location': $('#addUser fieldset input#inputUserLocation').val(),
+        'gender': $('#addUser fieldset input#inputUserGender').val()
+    }
+
+    // Use AJAX to post the object to our adduser service
+    $.ajax({
+        type: 'POST',
+        data: newUser,
+        url: '/users/adduser',
+        dataType: 'JSON'
+    }).done( function( response ) {
+        // Check for successful (blank) response
+        if ( response.msg === '' ) {
+            // Clear the form inputs
+            $( '#addUser fieldset input' ).val('');
+
+            // Update the table
+            populateTable();
+        }
+        else {
+            // If something goes wrong, alert the error message that our service returned
+            alert( 'Error: ' + response.msg );
+        }
+    });
 }
--- rest-api-node-express-mongo/routes/users.js
+++ rest-api-node-express-mongo/routes/users.js
@@ -4,9 +4,16 @@ var router = express.Router();
 // GET list of users from DB, return as JSON object
 // http://localhost:3000/users/userlist
 router.get( '/userlist' , function ( req , res ) {
-    req.db.collection( 'users').find().toArray( function( err , records ) {
+    req.db.collection( 'users' ).find().toArray( function( err , records ) {
         res.json( records );
     });
 });
+
+// POST from add user, insert into DB
+router.post( '/adduser' , function( req , res ) {
+    req.db.collection( 'users' ).insert( req.body , function( err , result ) {
+        res.send( { msg : ( err === null ? '' : err ) } );
+    });
+})
 
 module.exports = router;
