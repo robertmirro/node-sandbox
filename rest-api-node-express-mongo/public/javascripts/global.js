5ba14f3ddcc26f99f6bda92d86bfdc82ba9272f5
robertmirro <rmirro@gmail.com>
2014-08-12 17:44:30 -0400

    display user detail

--- rest-api-node-express-mongo/public/javascripts/global.js
+++ rest-api-node-express-mongo/public/javascripts/global.js
@@ -2,6 +2,9 @@ var userListData = [];
 
 $( document ).ready( function() {
     populateTable();
+
+    // attach click handler to all username links
+    $( '#userList table tbody' ).on( 'click' , 'td a.linkshowuser' , showUserInfo );
 });
 
 function populateTable() {
@@ -14,12 +17,36 @@ function populateTable() {
 
     $.getJSON( '/users/userlist' , function( data ) {
 //        console.log( 'json data:' , data );
+        // store user data globally just as a quick/dirty solution to persistence
+        userListData = data;
+
         $.each( data , function() {
             tableContent += tableRow.replace( /#USERNAME#/gi , this.username )
                 .replace( /#EMAIL#/gi , this.email )
                 .replace( /#_ID#/gi , this._id );
         });
 //        console.log( tableContent );
+
         $( '#userList table tbody' ).html( tableContent );
     });
+}
+
+function showUserInfo( event ) {
+    // prevent link from firing
+    event.preventDefault();
+
+    var thisUserName = $( this ).attr( 'rel' );
+
+    // map all usernames to array, find index of this username
+    var arrayIndex = userListData.map( function( userObject ) {
+        return userObject.username;
+    }).indexOf( thisUserName );
+    console.log( 'username: %s , index: %s' , thisUserName , arrayIndex );
+
+    var thisUserObject = userListData[ arrayIndex ];
+
+    $( '#userFullName' ).text( thisUserObject.fullname );
+    $( '#userAge' ).text( thisUserObject.age );
+    $( '#userGender' ).text( thisUserObject.gender );
+    $( '#userLocation' ).text( thisUserObject.location );
 }
