ca552205c720324a2acac14a3301b77a29510cc8
robertmirro <rmirro@gmail.com>
2014-08-17 17:37:14 -0400

    implement problem transform, pipe entire puzzle data 2d array to next transform

--- verify-suduko-with-streams/validate_suduko.js
+++ verify-suduko-with-streams/validate_suduko.js
@@ -9,32 +9,43 @@ var split = require('split');
 function transformStreamProblem() {
     var ts = stream.Transform( { objectMode : true } );
     ts._transform = function( dataLine , encoding , nextCb ) {
-        console.log( 'Problem:' , dataLine.match( /\d+/g ) );
+//        console.log( 'Problem:' , dataLine.match(  ) );
 
         if ( !this.totalPuzzlesToSolve ) {
+            // store total puzzles in case we want to validate input data
             this.totalPuzzlesToSolve = +dataLine;
         }
         else if ( !this.puzzleSize ) {
+            // collect data for a new puzzle
             this.puzzleSize = +dataLine * +dataLine;
-            this.puzzle2dArray = [];
+            this.puzzleData2dArray = [];
         }
         else {
-            ts.push( dataLine + ' (P)' /* + '\n' */ );
+            // populate 2d array with this line of puzzle data (an array of numbers)
+            this.puzzleData2dArray.push( dataLine.match( /\d+/g ) );
+            this.puzzleSize--;
+
+            // if all lines of this puzzle data have been processed
+            if ( this.puzzleSize === 0) {
+                // pipe puzzle data to next transform stream and prepare to process next set of puzzle data
+                ts.push( this.puzzleData2dArray );
+                this.puzzleSize = null;
+            }
         }
 
         nextCb();
     };
     ts.totalPuzzlesToSolve = null;
     ts.puzzleSize = null;
-    ts.puzzle2dArray = null;
+    ts.puzzleData2dArray = null;
     return ts;
 }
 
 function transformStreamSolution() {
     var ts = stream.Transform( { objectMode : true } );
     ts._transform = function( dataChunk , encoding , nextCb ) {
-        console.log( 'Solution:' , dataChunk );
-        ts.push( dataChunk + ' (S)' /* + '\n' */ );
+//        console.log( 'Solution:' , dataChunk );
+        ts.push( dataChunk /* + ' (S)' */ /* + '\n' */ );
         nextCb();
     };
     return ts;
@@ -44,7 +55,7 @@ function transformStreamFormat() {
     var ts = stream.Transform( { objectMode : true } );
     ts._transform = function( dataChunk , encoding , nextCb ) {
 //        console.log( 'Format:' , dataChunk );
-        ts.push( dataChunk + ' (F)' + '\n' );
+        ts.push( dataChunk  /* + ' (F)' */ + '\n' );
         nextCb();
     };
     return ts;
