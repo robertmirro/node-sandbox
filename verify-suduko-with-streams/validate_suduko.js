222b4accd2b70032179840c037a20d7cc443ab6f
robertmirro <rmirro@gmail.com>
2014-08-17 19:12:19 -0400

    finalize logic used to determine if puzzle is solved

--- verify-suduko-with-streams/validate_suduko.js
+++ verify-suduko-with-streams/validate_suduko.js
@@ -8,21 +8,21 @@ var split = require('split');
 
 function transformStreamProblem() {
     var ts = stream.Transform( { objectMode : true } );
-    ts._transform = function( dataLine , encoding , nextCb ) {
-//        console.log( 'Problem:' , dataLine.match(  ) );
+    ts._transform = function( inputLine , encoding , nextCb ) {
+//        console.log( 'Problem:' , inputLine.match(  ) );
 
         if ( !this.totalPuzzlesToSolve ) {
             // store total puzzles in case we want to validate input data
-            this.totalPuzzlesToSolve = +dataLine;
+            this.totalPuzzlesToSolve = +inputLine;
         }
         else if ( !this.puzzleSize ) {
             // collect data for a new puzzle
-            this.puzzleSize = +dataLine * +dataLine;
+            this.puzzleSize = +inputLine * +inputLine;
             this.puzzleData2dArray = [];
         }
         else {
             // populate 2d array with this line of puzzle data (an array of numbers)
-            this.puzzleData2dArray.push( dataLine.match( /\d+/g ) );
+            this.puzzleData2dArray.push( inputLine.match( /\d+/g ) );
             this.puzzleSize--;
 
             // if all lines of this puzzle data have been processed
@@ -43,23 +43,62 @@ function transformStreamProblem() {
 
 function transformStreamSolution() {
     var ts = stream.Transform( { objectMode : true } );
-    ts._transform = function( dataPuzzle , encoding , nextCb ) {
-//        console.log( 'Solution:' , dataPuzzle );
+    ts._transform = function( puzzle2DArray , encoding , nextCb ) {
+//        console.log( 'Solution:' , puzzle2DArray.length );
 
-        // TODO: Solve puzzle
-
-        ts.push( Math.round( Math.random() ) );
+        ts.push( puzzleIsSolved( puzzle2DArray ) );
         nextCb();
     };
     return ts;
 }
 
+function puzzleIsSolved( puzzle2dArray ) {
+    var puzzleSize = puzzle2dArray.length;
+
+    var correctSolution = 0;
+    for ( var i = 1; i <= puzzleSize; i++ ) {
+        correctSolution += i;
+//         console.log( 'i:%s = %s' , i , correctSolution );
+    }
+
+//    console.log( 'puzzleSize:%s , correctSolution:%s' , puzzleSize , correctSolution );
+
+    // solved horizontally
+    for ( var i = 0; i < puzzleSize; i++ ) {
+//         console.log( 'i:%s ' , i , puzzle2dArray[ i ] );
+        var puzzleSolution = puzzle2dArray[ i ].reduce( function( previousValue , currentValue , index , array ) {
+//            console.log( 'typeof previousValue:%s' , typeof previousValue );
+            return +previousValue + +currentValue;
+        });
+//        console.log( 'horizontal %s:%s' , i , puzzleSolution );
+        if ( puzzleSolution !== correctSolution ) {
+            return false;
+        }
+    }
+
+    // solved vertically
+    for ( var column = 0; column < puzzleSize; column++ ) {
+        var puzzleSolution = 0;
+        for ( var row = 0; row < puzzleSize; row++ ) {
+//             console.log( 'puzzle2dArray[row][column]:' , puzzle2dArray[row][column] );
+            puzzleSolution += +puzzle2dArray[row][column];
+        }
+//        console.log( 'vertical %s:%s' , column , puzzleSolution );
+        if ( puzzleSolution !== correctSolution ) {
+            return false;
+        }
+    }
+
+//    return Math.round( Math.random() );
+    return true;
+}
+
 function transformStreamFormat() {
     var ts = stream.Transform( { objectMode : true } );
-    ts._transform = function( dataSolution , encoding , nextCb ) {
-//        console.log( 'Format:' , dataSolution );
+    ts._transform = function( puzzleIsSolved , encoding , nextCb ) {
+//        console.log( 'Format:' , puzzleIsSolved , typeof puzzleIsSolved );
         this.puzzleNumber++;
-        ts.push( 'Puzzle# ' + this.puzzleNumber + ': ' + ( dataSolution ? 'Yes' : 'No' )  + '\n' );
+        ts.push( 'Puzzle# ' + this.puzzleNumber + ': ' + ( puzzleIsSolved ? 'Yes' : 'No' )  + '\n' );
         nextCb();
     };
     ts.puzzleNumber = 0;
