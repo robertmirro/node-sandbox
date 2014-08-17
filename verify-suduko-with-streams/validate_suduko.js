f6ad956beaf7bec6a94a2743c11e606176a13def
robertmirro <rmirro@gmail.com>
2014-08-17 17:18:44 -0400

    begin transforming input data

--- verify-suduko-with-streams/validate_suduko.js
+++ verify-suduko-with-streams/validate_suduko.js
@@ -7,17 +7,33 @@ var stream = require('stream');
 var split = require('split');
 
 function transformStreamProblem() {
-    var ts = stream.Transform();
-    ts._transform = function( dataChunk , encoding , nextCb ) {
-        ts.push( dataChunk + ' (P)' /* + '\n' */ );
+    var ts = stream.Transform( { objectMode : true } );
+    ts._transform = function( dataLine , encoding , nextCb ) {
+        console.log( 'Problem:' , dataLine.match( /\d+/g ) );
+
+        if ( !this.totalPuzzlesToSolve ) {
+            this.totalPuzzlesToSolve = +dataLine;
+        }
+        else if ( !this.puzzleSize ) {
+            this.puzzleSize = +dataLine * +dataLine;
+            this.puzzle2dArray = [];
+        }
+        else {
+            ts.push( dataLine + ' (P)' /* + '\n' */ );
+        }
+
         nextCb();
     };
+    ts.totalPuzzlesToSolve = null;
+    ts.puzzleSize = null;
+    ts.puzzle2dArray = null;
     return ts;
 }
 
 function transformStreamSolution() {
-    var ts = stream.Transform();
+    var ts = stream.Transform( { objectMode : true } );
     ts._transform = function( dataChunk , encoding , nextCb ) {
+        console.log( 'Solution:' , dataChunk );
         ts.push( dataChunk + ' (S)' /* + '\n' */ );
         nextCb();
     };
@@ -25,8 +41,9 @@ function transformStreamSolution() {
 }
 
 function transformStreamFormat() {
-    var ts = stream.Transform();
+    var ts = stream.Transform( { objectMode : true } );
     ts._transform = function( dataChunk , encoding , nextCb ) {
+//        console.log( 'Format:' , dataChunk );
         ts.push( dataChunk + ' (F)' + '\n' );
         nextCb();
     };
@@ -40,6 +57,7 @@ var tsFormat = transformStreamFormat();
 // set encoding so .toString() is not needed when transforming stream data chunks
 process.stdin.setEncoding( 'utf8' );
 
+// split() stdin data into lines and then transform each line one by one
 process.stdin
     .pipe( split() )
     .pipe( tsProblem )
