9dff9e06aa047880a1fd7b0ab1baddaa5b2825e4
robertmirro <rmirro@gmail.com>
2014-08-21 17:39:23 -0400

    refactor logic, add ObjectId() creiteria to find(), add findOne()

--- db-mongodb/mongodb_read_users.js
+++ db-mongodb/mongodb_read_users.js
@@ -5,23 +5,36 @@ mongodb.MongoClient.connect( 'mongodb://localhost:27017/node_test' , function( e
         throw err;
     }
 
-    console.log( 'Connected to %s...' , db.databaseName /*Object.keys( db )*/ );
+    console.log( '\nConnected to %s...' , db.databaseName /*Object.keys( db )*/ );
 //    console.log( Object.keys( db ) );
 //    console.log( db.collectionNames('users') );
 
     var collection = db.collection( 'users' );
 //    console.log( Object.keys( collection ) );
 
+    // true = find one , false = find all
+    var criteria = false ? { _id : new mongodb.ObjectID('53e7d8033584f6140cbe971a') } : {};
 
-    collection.find( {} , { username: 1 , email : 1 , _id : 0} ).toArray( function( err , documents ) {
+    collection.find( criteria  , { username: 1 , email : 1 , _id : 0} ).toArray( function( err , documents ) {
         collection.count( function( err , count ) {
             console.log( 'Total %s documents: %s' , collection.collectionName , count );
-            db.close();
+//            db.close();
         });
 
-        console.log( documents );
+        if ( documents && documents.length > 0 ) {
+            console.log( documents );
+        }
+
+        collection.findOne( {} , function( err , document ) {
+            console.log( '\nFinding one %s document...' , collection.collectionName );
+            if ( document ) {
+                console.log( document );
+                db.close();
+            }
+        });
+
 //        db.close();
     });
 
-    console.log( 'Finding %s documents...' , collection.collectionName );
+    console.log( '\nFinding %s documents...' , collection.collectionName );
 });
