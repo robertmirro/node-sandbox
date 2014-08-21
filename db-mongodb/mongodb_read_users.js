ae7a8aebe9dea74765b793aa17e5c77040bd9bfc
robertmirro <rmirro@gmail.com>
2014-08-21 17:21:41 -0400

    display collection count

--- db-mongodb/mongodb_read_users.js
+++ db-mongodb/mongodb_read_users.js
@@ -9,10 +9,19 @@ MongoClient.connect( 'mongodb://localhost:27017/node_test' , function( err , db 
 //    console.log( Object.keys( db ) );
 //    console.log( db.collectionNames('users') );
 
-    db.collection( 'users' ).find( {} , { username: 1 , email : 1 , _id : 0} ).toArray( function( err , documents ) {
+    var collection = db.collection( 'users' );
+//    console.log( Object.keys( collection ) );
+
+
+    collection.find( {} , { username: 1 , email : 1 , _id : 0} ).toArray( function( err , documents ) {
+        collection.count( function( err , count ) {
+            console.log( 'Total %s documents: %s' , collection.collectionName , count );
+            db.close();
+        });
+
         console.log( documents );
-        db.close();
+//        db.close();
     });
 
-    console.log( 'Finding users...' );
+    console.log( 'Finding %s documents...' , collection.collectionName );
 });
