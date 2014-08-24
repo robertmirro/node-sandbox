59990f79cfda6bd6b9462c96372b3873f363f3f0
robertmirro <rmirro@gmail.com>
2014-08-24 13:41:08 -0400

    convert to v2.0.0-alpha.4, re-compile external_templates.js

--- handlebars/external_template.js
+++ handlebars/external_template.js
@@ -1,20 +1,11 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['name'] = template(function (Handlebars,depth0,helpers,partials,data) {
-  this.compilerInfo = [4,'>= 1.0.0'];
-helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression;
-
-
-  buffer += "<div class=\"templateBodyItem\" style=\"display:none;\">\r\n    ";
-  if (helper = helpers.templateBodyItem) { stack1 = helper.call(depth0, {hash:{},data:data}); }
-  else { helper = (depth0 && depth0.templateBodyItem); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
-  buffer += escapeExpression(stack1)
-    + "\r\n    <div class=\"templateBodyItemFooter\">";
-  if (helper = helpers.templateBodyFooter) { stack1 = helper.call(depth0, {hash:{},data:data}); }
-  else { helper = (depth0 && depth0.templateBodyFooter); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
-  buffer += escapeExpression(stack1)
+templates['name'] = template({"compiler":[5,">= 2.0.0"],"main":function(depth0,helpers,partials,data) {
+  var helper, functionType="function", escapeExpression=this.escapeExpression;
+  return "<div class=\"templateBodyItem\" style=\"display:none;\">\r\n    "
+    + escapeExpression(((helper = helpers.templateBodyItem || (depth0 && depth0.templateBodyItem)),(typeof helper === functionType ? helper.call(depth0, {"name":"templateBodyItem","hash":{},"data":data}) : helper)))
+    + "\r\n    <div class=\"templateBodyItemFooter\">"
+    + escapeExpression(((helper = helpers.templateBodyFooter || (depth0 && depth0.templateBodyFooter)),(typeof helper === functionType ? helper.call(depth0, {"name":"templateBodyFooter","hash":{},"data":data}) : helper)))
     + "</div>\r\n</div>\r\n";
-  return buffer;
-  });
+},"useData":true});
 })();
