708775c9f6ecd023c8a91176c0a880f082e86b05
robertmirro <rmirro@gmail.com>
2014-08-25 20:22:36 -0400

    comment command line usage of mongo shell

--- db-mongodb-m101js/source/async_vs_sync/script.js
+++ db-mongodb-m101js/source/async_vs_sync/script.js
@@ -1,4 +1,9 @@
 
+// run from command line:
+// 
+//   dev /c/Dev/GitRepos/node-sandbox/db-mongodb-m101js (master)
+//   $ mongo node_demo ./source/async_vs_sync/script.js
+
 // Find one document in our collection
 var doc = db.contacts.findOne();
 
