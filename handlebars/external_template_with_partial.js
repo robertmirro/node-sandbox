2db08d0b52a475eea6c8fd53e7ef51976e423af7
robertmirro <rmirro@gmail.com>
2014-08-26 19:31:05 -0400

    #each skillz

--- handlebars/external_template_with_partial.js
+++ handlebars/external_template_with_partial.js
@@ -3,8 +3,18 @@
 templates['name_with_partial'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); partials = this.merge(partials, Handlebars.partials); data = data || {};
-  var buffer = "", stack1, helper, self=this, functionType="function", escapeExpression=this.escapeExpression;
+  var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression, self=this;
 
+function program1(depth0,data) {
+  
+  var buffer = "", stack1, helper;
+  buffer += "\r\n        <div class=\"templateBodySkillz\">";
+  if (helper = helpers.skill) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.skill); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  buffer += escapeExpression(stack1)
+    + "</div>\r\n    ";
+  return buffer;
+  }
 
   buffer += "\r\n<!-- handlebars template - name -->\r\n<div class=\"templateBodyItem\" style=\"display:none;\">\r\n    ";
   stack1 = self.invokePartial(partials.templateBodyItemNumberPartial, 'templateBodyItemNumberPartial', depth0, helpers, partials, data);
@@ -12,7 +22,10 @@ helpers = this.merge(helpers, Handlebars.helpers); partials = this.merge(partial
   if (helper = helpers.templateBodyItem) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.templateBodyItem); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "\r\n    <div class=\"templateBodyItemFooter\">";
+    + "\r\n    ";
+  stack1 = helpers.each.call(depth0, (depth0 && depth0.templateBodySkillz), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\r\n    <div class=\"templateBodyItemFooter\">";
   if (helper = helpers.templateBodyFooter) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.templateBodyFooter); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
