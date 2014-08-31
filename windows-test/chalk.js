d9f23df7cf330cdc9bab1e3b74f8ec70d16a82a2
robertmirro <rmirro@gmail.com>
2014-08-31 18:09:03 -0400

    chalk.js

---
+++ windows-test/chalk.js
@@ -0,0 +1,24 @@
+var chalk = require('chalk');
+
+// style a string
+console.log(  chalk.blue('Hello world!')  );
+
+// combine styled and normal strings
+console.log(  chalk.blue('Hello'), 'World' + chalk.red('!')  );
+
+// compose multiple styles using the chainable API
+console.log(  chalk.blue.bgRed.bold('Hello world!')  );
+
+// pass in multiple arguments
+console.log(  chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz')  );
+
+// nest styles
+console.log(  chalk.red('Hello', chalk.underline.bgBlue('world') + '!')  );
+
+// nest styles of the same type even (color, underline, background)
+console.log(  chalk.green('I am a green line ' + chalk.blue('with a blue substring') + ' that becomes green again!')  );
+
+var error = chalk.bold.red;
+console.log(error('Alert! Error! %s') , 'SERIOUS ERROR!!!');
+
+console.log( chalk.white.bgMagenta.bold.strikethrough('Yikes!') );
