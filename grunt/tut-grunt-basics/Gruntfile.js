e4d7655e5076f935b27ac87c3adb595b10ffacf5
robertmirro <rmirro@gmail.com>
2014-09-05 18:57:12 -0400

    experiment with options

--- grunt/tut-grunt-basics/Gruntfile.js
+++ grunt/tut-grunt-basics/Gruntfile.js
@@ -5,8 +5,27 @@ module.exports = function (grunt) {
 				files: {
 					'calculator.min.js': 'calculator/*.js'
 				},
+				/* NOTES:
+					- footer seems to have no effect
+					- sourceMaps seem to be broken
+				*/
 				options: {
-					banner: '/* This is my minified app, built <%= grunt.template.today() %> */'
+					banner: '/* This is my minified app, built <%= grunt.template.today() %> */\n' ,
+					footer: '\n/* footer... */\n' ,
+					compress: {
+						/* none of the options below are working: http://stackoverflow.com/a/21020309 */
+						/* drop_console: true */
+						/* dropConsole: true */
+						/* pure_funcs: ['console.log'] */
+					} ,
+					beautify: false ,
+					preserveComments: false ,
+					
+					/* this is NOT working as expected:
+					sourceMap: true ,
+					sourceMapName: 'calculator.map'
+					*/
+					sourceMap: 'calculator.map'
 				}
 			},
 			addsub: {
--- grunt/tut-grunt-basics/calculator/subtract.js
+++ grunt/tut-grunt-basics/calculator/subtract.js
@@ -1,3 +1,5 @@
+/* subtract */
 function subtract (x, y) {
+  console.log( 'subtract %s - %s' , x , y );  
   return x - y;
 }
