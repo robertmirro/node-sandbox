987f45df6f441acdc025d0976f843a509dd7c8cc
robertmirro <rmirro@gmail.com>
2014-09-08 17:20:28 -0400

    comment with shell version of command

--- db-mongodb-m101js/source/week_2/hw2-1/hw2-1.js
+++ db-mongodb-m101js/source/week_2/hw2-1/hw2-1.js
@@ -4,6 +4,8 @@
     var total = 0;
     var projection = { State : 1 , Temperature : 1 };
 
+    // from shell: db.data.find( { "Wind Direction" : { $gte : 180 , $lte : 360 } } ).sort( {Temperature : 1 } ).limit(1).pretty()
+
     db.data.find( { "Wind Direction" : { $gte : 180 , $lte : 360 } } , projection )
         .sort( { Temperature : 1 } )
         .limit( 1 )
