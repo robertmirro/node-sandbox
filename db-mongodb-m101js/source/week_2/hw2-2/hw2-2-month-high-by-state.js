3c5e97064819842a16f613c99c00691ae65275bf
robertmirro <rmirro@gmail.com>
2014-09-08 21:18:41 -0400

    implement setInterval logic to handle async db.close()

--- db-mongodb-m101js/source/week_2/hw2-2/hw2-2-month-high-by-state.js
+++ db-mongodb-m101js/source/week_2/hw2-2/hw2-2-month-high-by-state.js
@@ -4,32 +4,58 @@ var mongodb = require('mongodb') ,
 
 MongoClient.connect( 'mongodb://localhost:27017/weather' , function( err , db ) {
 
-    var sortBy = [ [ 'State' , 1 ] , [ 'Temperature' , -1 ] ];
-    var currentState = '';
+    function closeDb() {
+        console.log( 'closing db...' );    
+        db.close();
+    }
+
+    function logError( errType , err ) {
+        console.log( 'ERROR : %s : %s' , errType , err.message );
+        closeDb();        
+    }
+
+    var sortBy = [ [ 'State' , 1 ] , [ 'Temperature' , -1 ] ] , 
+        currentState = '' , 
+        pendingOperations = 0 , 
+        maxCloseAttempts = 10 ;
 
     db.collection( 'data' ).find().sort( sortBy ).each( function( err , doc ) {
         if ( err ) {
-            console.log( 'error 1' );
+            logError( 'find()' , err );
             throw err;
         }
         if ( doc == null ) {
-            // console.log( 'closing db 1...' );
-            // return db.close();
-
-            setTimeout( function() {
-                console.log( 'closing db 2...' );    
-                db.close();
+            var intervalID = setInterval( function() {
+                if ( pendingOperations <= 0 || maxCloseAttempts <= 0) {
+                    closeDb();
+                    clearInterval( intervalID );
+                } else {
+                    console.log( 'waiting to close db, pending operations:' , pendingOperations );
+                    maxCloseAttempts -= 1;
+                }
             } , 0 );
+
+            // setTimeout( function() {
+            //     console.log( 'closing db 2...' );    
+            //     db.close();
+            // } , 0 );
 
             return;
         }
 
         if ( currentState !== doc.State ) {
             currentState = doc.State;
+            pendingOperations += 1;
             // console.log( 'state: %s , temp: %s' , doc.State , doc.Temperature );
 
             // find data in shell: db.data.find( { month_high : true} ).pretty()
             // undo updates in shell: db.data.update( { month_high : true} , { $unset : { month_high : true } } , { multi : true } )
+
+            // VALIDATE HOMEWORK:
+            //
+            // $ node validate.js
+            // hw2-2 Validated successfully!
+            // Your validation code is: WSRMOzQ4KTKhFEC6rUDn
 
             var query = { _id: ObjectID( doc._id ) } ,
                 operator = { $set : { month_high : true } } ,
@@ -39,11 +65,12 @@ MongoClient.connect( 'mongodb://localhost:27017/weather' , function( err , db ) 
 
             db.collection( 'data' ).findAndModify( query , sort , operator , options , function ( err , doc ) {
                 if ( err ) {
-                    console.log( 'error 3' );
+                    logError( 'findAndModify()' , err );
                     throw err;
                 }
 
                 console.log( 'updated: %s : %s : %s' , doc.State, doc.Temperature , doc._id );
+                pendingOperations -= 1;
             });   
 
             // db.collection( 'data' ).update( query , operator , function( err , updated ) {
