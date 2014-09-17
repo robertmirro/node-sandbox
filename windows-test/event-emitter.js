59c923746ba162593469391ae8c4367a3a82a89c
robertmirro <rmirro@gmail.com>
2014-09-17 12:42:32 -0400

    event emitter, bind() emit to this

--- windows-test/event-emitter.js
+++ windows-test/event-emitter.js
@@ -8,16 +8,19 @@ function MyClass() {
     EventEmitter.call(this);
 
     var self = this;
-    //setTimeout(function timeoutCb() {
     setTimeout(function () {
         self.emit('myEvent', 'hello world', 44, 84);
     }, 1000);
+
+    setTimeout(function () {
+        this.emit('myEvent', 'hello again world', 4, 8);
+    }.bind( this ), 2000);
 }
 util.inherits(MyClass, EventEmitter);
 
 var myObj = new MyClass();
 var start = Date.now();
 myObj.on('myEvent', function myEventCb(str, num, num2) {
-    console.log('myEvent triggered: ', str, num, num2, Date.now() - start);
+    console.log('myEvent triggered:', str, num, num2, Date.now() - start);
 });
 
