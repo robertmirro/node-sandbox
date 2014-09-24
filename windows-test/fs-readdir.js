f447f1a5d571f336df48527c23ba7cff5e6d3fda
robertmirro <rmirro@gmail.com>
2014-09-24 19:07:07 -0400

    include async version of listing file stats

--- windows-test/fs-readdir.js
+++ windows-test/fs-readdir.js
@@ -1,15 +1,18 @@
 var fs = require('fs') ,
-    path = require('path');
+    path = require('path') ,
+    async = require('async');
 
 fs.readdir( __dirname , function( err , files ) {
     if ( err ) {
         throw err;
     }
 
-    console.log( '\nDirectory Listing (total: %s):\n%s\n' , files.length, __dirname );
-
     // console.log( files );
     files.forEach( function( file , index , array ) {
+        if ( index === 0 ) {
+            console.log( '\nDirectory Listing - forEach (total: %s):\n%s\n' , files.length, __dirname );
+        }
+
         fs.stat( path.join( __dirname ,  file ) , function( err , stats) {
             console.log( 
                 ' %s - %s %s' , 
@@ -19,4 +22,27 @@ fs.readdir( __dirname , function( err , files ) {
             );
         });
     });
+
+
+    function getFileStats( file , cb ) {
+        fs.stat( file , function( err , stats ) {
+            cb( err , { 'file' : file , 'stats' : stats } );
+        });
+    }
+
+    async.map( files , getFileStats /* fs.stat */ , function( err , fileStats ) {
+        console.log( '\nDirectory Listing - async.map (total: %s):\n%s\n' , files.length, __dirname );
+        
+        // console.log( fileStats[0] );
+
+        fileStats.forEach( function( stat , index , array ) {
+            console.log( 
+                ' %s - %s %s' , 
+                stat.stats.isFile() ? 'F' : 'D' , 
+                stat.file ,
+                stat.stats.isFile() ? '[' + stat.stats.size + ' bytes]' : ''
+            );
+        });
+    });
+
 });   
