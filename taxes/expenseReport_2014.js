262d8daddb9cf3f3f07d741fb19c382167f928cb
robertmirro <rmirro@gmail.com>
2015-04-12 19:34:41 -0400

    taxes - 2014

---
+++ taxes/expenseReport_2014.js
@@ -0,0 +1,87 @@
+// http://tagtree.tv/intro-to-node-streams
+// words.txt - https://raw.githubusercontent.com/eneko/data-repository/master/data/words.txt
+
+"use strict";
+
+var fs = require('fs');
+var stream = require('stream');
+var moment = require('moment');
+
+console.log( moment('01\\10\\1971', 'MM-DD-YYYY').toDate().getTime() );
+
+
+
+// if maxWords is not passed, read stream will continue indefinitely
+function randomWordStream( maxWords ) {
+    var rs = stream.Readable();
+    var wordContents = fs.readFileSync( './Expenses 2014 new.txt' , 'utf8' );
+// console.log('wordContents:', wordContents);    
+//    console.log( '%s bytes' , wordContents.length );
+    var words = wordContents.split( '\n' );
+// console.log('words:', words);    
+   console.log( '%s words' , words.length );
+    var currentWord = 0;
+
+    rs._read = function( size ) {
+console.log('_read...');        
+        if ( currentWord >= maxWords ) {
+            // null terminator to inform consumer that data is done being output
+            return rs.push( null );
+        }
+
+        currentWord++;
+
+        // simulate a delay and illustrate async processing
+        setTimeout( function() {
+            var randomIndex = Math.floor( Math.random() * words.length ) ;
+            rs.push( currentWord + '. ' + words[ randomIndex ] /* + '\n' */ );
+        }, 100 );
+    };
+
+    // return an instance of our stream
+    return rs;
+}
+
+function writeWordStream() {
+    var ws = stream.Writable();
+    ws._write = function( dataChunk , encoding , nextCb ) {
+        console.log( dataChunk.toString() );
+
+        // simulate a delay and illustrate async processing
+        // inform producer we are ready for next dataChunk
+        // nextCb();
+        setTimeout( nextCb , 100 );
+    };
+    return ws;
+}
+
+// act as BOTH a read AND write stream
+function transformWordStream() {
+    var ts = stream.Transform();
+    ts._transform = function( dataChunk , encoding , nextCb ) {
+//        console.log( 'chunk: %s\n' , dataChunk.toString() + '(' + ')' );
+        // transform pass-thru data to UPPERCASE and push it out to write stream
+        // display original word text in parens (split word from index + word)
+        var wordString = dataChunk.toString().trim();
+        ts.push( wordString.toUpperCase() + ' (' + wordString.split(' ')[1] + ')' );
+
+        // simulate a delay and illustrate async processing
+        // inform producer we are ready for next dataChunk
+        // nextCb();
+        setTimeout( nextCb , 100 );
+    };
+    return ts;
+}
+
+// pass maxWords as a command line arg
+var rws = randomWordStream( process.argv[2] );
+console.log('AFTER randomWordStream');
+var wws = writeWordStream();
+var tws = transformWordStream();
+// rws.pipe( tws ).pipe( wws );
+
+// test - display 10 words:
+// $ node readable_writable_transform_stream_random_word.js 10
+//
+// test - display words infinitely -
+// $ node readable_writable_transform_stream_random_word.js
