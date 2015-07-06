5780bccf576af49e28747927fa218eaaa0353ba6
robertmirro <rmirro@gmail.com>
2015-07-06 18:11:15 -0400

    plumber and autoprefixer

--- gulp-build-system/gulpFile.js
+++ gulp-build-system/gulpFile.js
@@ -3,23 +3,29 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
-    compass = require('gulp-compass');
+    compass = require('gulp-compass'),
+    plumber = require('gulp-plumber'),
+    autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('scripts', function() {
     gulp.src(['app/js/**/*.js', '!app/js/**/*.min.js'])
+        .pipe(plumber())
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
         .pipe(gulp.dest('app/js'));
 });
 
+// NOTE: plumber NEEDS to be located directly after .src()
 gulp.task('compass', function() {
     gulp.src('app/scss/style.scss')
+        .pipe(plumber())
         .pipe(compass({
             config_file: './config.rb',
             css: 'app/css',
             sass: 'app/scss',
             require: ['susy']
         }))
+        .pipe(autoprefixer('last 2 versions'))
         .pipe(gulp.dest('app/css/'));
 });
 
