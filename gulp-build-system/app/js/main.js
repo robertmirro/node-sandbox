6eb166f3607ff2dedf7bbcb84e190022bb9be581
robertmirro <rmirro@gmail.com>
2015-07-06 17:24:13 -0400

    gulp rename, uglify

---
+++ gulp-build-system/app/js/main.js
@@ -0,0 +1,13 @@
+;(function() {
+    'use strict';
+
+    var myVar = 'testing 1, 2, 4...';
+    console.log('inside of main.js: myVar:', myVar);
+
+    function test() {
+        console.log('test...');
+    }
+    test();
+
+})();
+
--- gulp-build-system/gulpFile.js
+++ gulp-build-system/gulpFile.js
@@ -1,4 +1,14 @@
+'use strict';
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
 
-gulp.task('default', []);
+gulp.task('scripts', function() {
+    gulp.src(['app/js/**/*.js', '!app/js/**/*.min.js'])
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/js'));
+});
+
+gulp.task('default', ['scripts']);
