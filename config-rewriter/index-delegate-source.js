49b61b2ff4a4b917169a440e799ec3accab04579
robertmirro <rmirro@evolenthealth.com>
2018-12-04 15:04:51 -0500

    cleanup

--- config-rewriter/index-delegate-source.js
+++ config-rewriter/index-delegate-source.js
@@ -14,7 +14,7 @@ const cleanseArgs = [];
 const configFile = './eh-config.js';
 let config = fs.readFileSync(configFile, 'utf8');
 
-parseScript(config, parseOptions, (node, meta) => {
+parseScript(config, parseOptions, node => {
     if (node.type === Syntax.CallExpression && node.callee.name === 'vsts' && !/^__\w*__$/.test(node.arguments[0].value)) {
         const [start, end] = node.arguments[1].range;
         cleanseArgs.push({
